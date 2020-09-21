0d8ce9a564b3cd3bfd78738ba230f868de540461
Anas Awadalla <anasa2@cs.washington.edu>
2020-09-20 22:15:31 -0700

    restored graph creation build

--- mmdnn/conversion/keras/keras2_graph.py
+++ mmdnn/conversion/keras/keras2_graph.py
@@ -3,7 +3,7 @@
 #  Licensed under the MIT License. See License.txt in the project root for license information.
 # ----------------------------------------------------------------------------------------------
 import os
-import tensorflow.keras as _keras
+import keras as _keras
 from mmdnn.conversion.common.DataStructure.graph import GraphNode, Graph
 
 
@@ -29,9 +29,8 @@ class Keras2Graph(Graph):
 
     def __init__(self, model):
         # sanity check.
-        print(model.summary())
-        # if not (type(model) == _keras.models.Sequential or type(model) == _keras.models.Model):
-        #     raise TypeError("Keras layer of type %s is not supported." % type(model))
+        if not (type(model) == _keras.models.Sequential or type(model) == _keras.models.Model):
+            raise TypeError("Keras layer of type %s is not supported." % type(model))
         super(Keras2Graph, self).__init__(model)
         self.model = model
 
@@ -40,13 +39,12 @@ class Keras2Graph(Graph):
         for i, layer in enumerate(self.model.layers):
             self.layer_map[layer.name] = Keras2GraphNode(layer)
             self.layer_name_map[layer.name] = layer.name
-            print(layer)
             for node in layer._inbound_nodes:
-                # for pred in node.inbound_layers:
-                if node.name not in self.layer_map:
-                    self.layer_map[node.name] = Keras2GraphNode(node)
-                    self.layer_name_map[node.name] = node.name
-                self._make_connection(node.name, layer.name)
+                for pred in node.inbound_layers:
+                    if pred.name not in self.layer_map:
+                        self.layer_map[pred.name] = Keras2GraphNode(pred)
+                        self.layer_name_map[pred.name] = pred.name
+                    self._make_connection(pred.name, layer.name)
 
         # Kit: TODO
         # Duplicate models for weight sharing
