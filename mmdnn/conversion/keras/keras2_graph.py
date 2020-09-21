d7d2b5e37c47cfd8fd625b778b55badc505444ad
Anas Awadalla <anasa2@cs.washington.edu>
2020-09-20 21:49:02 -0700

    added debug

--- mmdnn/conversion/keras/keras2_graph.py
+++ mmdnn/conversion/keras/keras2_graph.py
@@ -41,11 +41,11 @@ class Keras2Graph(Graph):
             self.layer_map[layer.name] = Keras2GraphNode(layer)
             self.layer_name_map[layer.name] = layer.name
             for node in layer._inbound_nodes:
-                for pred in node.inbound_layers:
-                    if pred.name not in self.layer_map:
-                        self.layer_map[pred.name] = Keras2GraphNode(pred)
-                        self.layer_name_map[pred.name] = pred.name
-                    self._make_connection(pred.name, layer.name)
+                # for pred in node.inbound_layers:
+                    if node.name not in self.layer_map:
+                        self.layer_map[node.name] = Keras2GraphNode(node)
+                        self.layer_name_map[node.name] = node.name
+                    self._make_connection(node.name, layer.name)
 
         # Kit: TODO
         # Duplicate models for weight sharing
