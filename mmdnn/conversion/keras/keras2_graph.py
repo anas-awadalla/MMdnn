152745dd49ba7d4189193852103e8894553822f7
Anas Awadalla <anasa2@cs.washington.edu>
2020-09-20 21:27:34 -0700

    added debug

--- mmdnn/conversion/keras/keras2_graph.py
+++ mmdnn/conversion/keras/keras2_graph.py
@@ -1,7 +1,7 @@
-#----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License. See License.txt in the project root for license information.
-#----------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
 import os
 import keras as _keras
 from mmdnn.conversion.common.DataStructure.graph import GraphNode, Graph
@@ -12,33 +12,27 @@ class Keras2GraphNode(GraphNode):
     def __init__(self, layer):
         super(Keras2GraphNode, self).__init__(layer)
 
-
     @property
     def name(self):
         return self.layer.name
 
-
     @property
     def type(self):
         return self.layer.__class__.__name__
-
 
     @property
     def keras_layer(self):
         return self.layer
 
 
-
 class Keras2Graph(Graph):
 
     def __init__(self, model):
         # sanity check.
-        print(model)
-        if not (type(model) == _keras.models.Sequential or type(model) == _keras.models.Model):
-            raise TypeError("Keras layer of type %s is not supported." % type(model))
+        # if not (type(model) == _keras.models.Sequential or type(model) == _keras.models.Model):
+        #     raise TypeError("Keras layer of type %s is not supported." % type(model))
         super(Keras2Graph, self).__init__(model)
         self.model = model
-
 
     def build(self):
         self.input_layers = list()
